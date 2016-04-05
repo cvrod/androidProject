c92326ecad915ec90a39d7a807235afc7afd39f7
Camille Vrod <cvrod@excilys.com>
2016-04-05 12:35:43 +0200

    feat(MainActivity): MainActivity WIP

---
+++ app/src/main/java/com/excilys/parlezvous/projetandroid/ConnectionTask.java
@@ -0,0 +1,74 @@
+package com.excilys.parlezvous.projetandroid;
+
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by excilys on 05/04/16.
+ */
+public class ConnectionTask extends android.os.AsyncTask {
+    MainActivity activity;
+    String user;
+    String password;
+    boolean identification = false;
+
+    public ConnectionTask(MainActivity act){
+        this.activity = act;
+    }
+
+    protected void onPreExecute(){
+        super.onPreExecute();
+        ProgressBar bar = activity.getProgressBar();
+        bar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        user = activity.getUsername();
+        password = activity.getPassword();
+
+        System.out.println("User : "+user);
+        System.out.println("Password : "+password);
+
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            //url = new URL("http://[host]:[port]/connect/"+user+"/"+password);
+            url = new URL("http://www.google.com");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            String response = InputStreamToString.convert(in);
+            System.out.println(response);
+            //if(response.compareTo("true") == 0){
+            //TODO
+            if(!response.isEmpty()){
+                activity.setIdentificationSuccess(true);
+                return true;
+            }
+            else{
+                Toast.makeText(activity, "Utilisateur inconnu !", Toast.LENGTH_SHORT).show();
+                activity.setIdentificationSuccess(false);
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+        return null;
+    }
+
+    protected void onPostExecute(Object result) {
+        super.onPostExecute(result);
+        ProgressBar bar = activity.getProgressBar();
+        bar.setVisibility(View.INVISIBLE);
+    }
+}
--- app/src/main/java/com/excilys/parlezvous/projetandroid/MainActivity.java
+++ app/src/main/java/com/excilys/parlezvous/projetandroid/MainActivity.java
@@ -1,52 +1,136 @@
 package com.excilys.parlezvous.projetandroid;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    //Getting class name
+    private final String TAG = MainActivity.class.getSimpleName();
+    public static final String PREFS_NAME = "PrefsFile";
+
+    private EditText usernameField;
+    private EditText passwordField;
+
+    private Button flushButton;
+    private Button sendButton;
+
+    private ProgressBar progressBar;
+
+    private boolean identificationSuccess = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Log.i(TAG, "onCreate !");
+        usernameField = (EditText) findViewById(R.id.user);
+        passwordField = (EditText) findViewById(R.id.password);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        flushButton = (Button) findViewById(R.id.viderButton);
+        sendButton = (Button) findViewById(R.id.envoyerButton);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        //Setting user/password from SharedPreferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        String user = settings.getString("user", "");
+        String password = settings.getString("password", "");
+
+        usernameField.setText(user);
+        passwordField.setText(password);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    //Getters
+    public ProgressBar getProgressBar() {
+        return this.progressBar;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public String getUsername() {
+        return this.usernameField.getText().toString();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public String getPassword() {
+        return this.passwordField.getText().toString();
+    }
+
+    //Setters
+    public void setIdentificationSuccess(boolean b) {
+        this.identificationSuccess = b;
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy !");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause !");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume !");
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState !");
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState !");
+    }
+
+    //Reset user/password textfield
+    public void flushButtonMethod(View view) {
+        usernameField.setText("");
+        passwordField.setText("");
+    }
+
+    //Sending logs
+    public void sendButtonMethod(View view) {
+        Toast.makeText(this, "Toast !", Toast.LENGTH_SHORT).show();
+        ConnectionTask task = new ConnectionTask(this);
+        task.execute();
+        boolean result = false;
+        try {
+            result = (boolean) task.get();
+            Log.i(TAG, "Result from ParlezVousTask : " + result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        //if ParlezVousTask return true -> login exist
+        if (result) {
+            //Putting user/password in Preferences
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("user", this.getUsername());
+            editor.putString("password", this.getPassword());
+            editor.commit();
+
+            //TODO
+            //Intent intent = new Intent(MainActivity.this, ConnectedActivity.class);
+            //intent.putExtra("user", usernameField.getText().toString());
+            //startActivity(intent);
+        }
+
     }
 }
